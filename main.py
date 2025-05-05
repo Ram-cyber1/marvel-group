40651ffce98bb3e189f79aee30b69944afc8d897
Ram Sharma <ramsharma231104@gmail.com>
2025-05-05 21:28:45 +0530

    m3

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 
 # Hugging Face model endpoints
 IMAGE_CAPTIONING_MODEL = "Salesforce/blip-image-captioning-large"
-IMAGE_GENERATION_MODEL = "stabilityai/stable-diffusion-3.5-large"
+IMAGE_GENERATION_MODEL = "runwayml/stable-diffusion-v1-5"
 
 # OCR.Space API configuration
 OCR_SPACE_API_URL = "https://api.ocr.space/parse/image"
