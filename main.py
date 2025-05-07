f511a2f7ea628945909f2e3ad28e885eb9858e7b
Ram Sharma <ramsharma231104@gmail.com>
2025-05-07 20:04:21 +0530

    pro 3

--- main.py
+++ main.py
@@ -1162,6 +1162,7 @@ async def reset_context(request: Request):
 
 
 
-if name == "main":
+if __name__ == "__main__":
+
     port = int(os.getenv("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
